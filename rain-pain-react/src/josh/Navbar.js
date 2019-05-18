6e39f5a6c128e28b56f42a63f475288b501f7f15
Joshua Frasier <jafrasier50@hotmail.com>
2019-05-18 15:02:46 -0500

    i have a navbar
    made navbar reverse colors

---
+++ rain-pain-react/src/josh/Navbar.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
+
+export default class Example extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar color="dark" dark expand="md">
+                    <NavbarBrand href="/">RainPain</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/components/">About</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="">Methodology</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="">Team</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="">Contact</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
