c7ac47dff68f03ecac200bdb66000b2f23b21e23
Your Name <jamesjosephsewell@gmail.com>
2019-05-19 11:09:34 -0500

    deploy test

--- rain-pain-react/src/App.js
+++ rain-pain-react/src/App.js
@@ -2,17 +2,13 @@ import React from "react";
 import "./App.css";
 import { Switch, Router, Route } from "react-router";
 import { createBrowserHistory } from "history";
-import { Home } from "./james/home/home"
+import { Home } from "./james/home/home";
 
 const history = createBrowserHistory();
 
 function App() {
   return (
-    <Router history={history}>
-      <Switch>
-        <Route exact path="/" component={Home}/>
-      </Switch>
-    </Router>
+    <Home />
   );
 }
 
